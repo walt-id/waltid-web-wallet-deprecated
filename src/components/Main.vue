d244eb110b11f6b82e6b111550192e8b824dbcea
Othmane N <otmannj56@gmail.com>
2021-10-27 14:49:52 +0100

    vue starter, extend node modules for aniimations

---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+ <h1>Project starter</h1>
+</template>
+
+<script>
+     export default{
+         name: 'Main'
+     }
+</script>
+
+<style scoped>
+ h1{
+     color: green
+ }
+</style>
