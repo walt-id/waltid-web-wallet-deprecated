ad0fde4eb5679263132dec38ecab7e7f59d4788e
Philipp Potisk <phil@walt.id>
2021-10-29 19:33:48 +0200

    Restructured user-flow

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -13,10 +13,11 @@
                     </div>
                     <div class="my-2">
                         <button type="submit" name="submit" class="text-white border-0 rounded">Login</button>
+                       <br> <router-link to="/credential-request">should go to => Credential Request</router-link>
                     </div>
                     <div class="my-2 d-flex mt-4 justify-content-center">
                         <a href="/signup.html" class="px-3 py-0">Sign up</a>
-                        <a href="https://loving-curran-95bebe.netlify.app/" class="px-3 py-0 border-start border-2 ">Forgot password?</a>
+                        <a href="/forgot-password.html" class="px-3 py-0 border-start border-2 ">Forgot password?</a>
                     </div>
                 </form>
                 <a id="copyright" href="https://walt.id/" target="_blank">by walt.id</a>
