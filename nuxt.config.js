8c72521362e5be05717300c0d953b5d8acfa09c7
Othmane N <otmannj56@gmail.com>
2021-11-04 01:34:54 +0100

    Check Build

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '@/plugins/bootstrap-vue'
+    '@/plugins/bootstrap'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
