587563897a99b96b3d57a7b852089e76cef55b26
Severin Stampler <severin@walt.id>
2021-11-11 12:42:01 +0100

    page for receiving credential from issuer

--- pages/ReceiveCredential.vue
+++ pages/ReceiveCredential.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="_main bg-light row align-items-center justify-content-center justify-content-lg-start justify-content-md-center justify-content-sm-center p-0">
+      <section class="_main bg-light row align-items-center justify-content-center justify-content-lg-start justify-content-md-center justify-content-sm-center p-0">
         <div id="widget" class="_form d-black bg-w shadow-lg text-center">
             <div class="_toggle-menu position-sticky d-flex justify-content-end col-12 align-items-center px-3">
                 <a id="toggle-menu" @click="menuTrigger">
@@ -9,7 +9,7 @@
                 </a>
             </div>
             <div class="_content justify-content-center d-flex align-items-center ">
-                <div id="content">
+                <div id="content-confirm" v-if="!receivedCredentials && !selectedCredential">
                     <h2>Confirm credential issuance</h2>
                     <div class="_scrollable _container d-flex flex-column align-items-center justify-content-center">
                         <div class="col-10 d-flex align-items-center">
@@ -23,6 +23,43 @@
                             <a href="#reject" class="_reject col-12">Reject</a>
                         </div>
                     </div>
+                </div>
+                <div id="content-credentials" v-if="receivedCredentials && !selectedCredential">
+                  <h2>Received credentials</h2>
+                  <div class="_scrollable _container d-flex flex-column align-items-center justify-content-center">
+                    <a v-for="credential in receivedCredentials" :key="credential.id"
+                       class="_card d-flex _animation-fade" href="#selectCredential"
+                       @click="selectedCredential = credential">
+                      <div class="col-10 d-flex align-items-center">
+                        <div>
+                          <h5 class="mb-1">{{credential.title ? credential.title : credential.type[credential.type.length-1]}}</h5>
+                          <p class="text-truncate" style="max-width: 12em">by {{credential.issuerName ? credential.issuerName : credential.issuer}}</p>
+                        </div>
+                      </div>
+                    </a>
+                    <div class="_button">
+                      <button href="#share" class="_share col-12 mb-2" @click="$router.push('/Credentials')">Accept</button>
+                      <a href="#reject" class="_reject col-12">Reject</a>
+                    </div>
+                  </div>
+                </div>
+                <div v-if="selectedCredential">
+                  <div class="_top-bar  position-sticky d-flex justify-content-center col-12 align-items-center px-3">
+                    <div class="col-12 d-flex align-items-center _animation-fade">
+                      <a href="" @click.prevent="selectedCredential = null" class="_back-button">
+                        <span class="d-flex align-items-center">
+                            <i class="bi bi-chevron-left"></i>
+                            <p>Back</p>
+                        </span>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="_window d-flex justify-content-center align-items-center">
+                    <div class="_window-content m-2 p-2">
+                      <CredentialView :credential="selectedCredential" class="p-4">
+                      </CredentialView>
+                    </div>
+                  </div>
                 </div>
                 <div id="menu-content" class="_menu-content hide">
                     <ul>
@@ -57,13 +94,15 @@ export default {
   data() {
     return {
       trigger: true,
-      id: 'xxxxxxxxxx'
+      id: 'xxxxxxxxxx',
+      receivedCredentials: null,
+      selectedCredential: null
     }
   },
   async asyncData ({ $axios, query }) {
     // TODO: select DID to use
     const dids = await $axios.$get("/api/wallet/did/list")
-    const pe = await $axios.$get("/api/wallet/siopv2/presentationExchange", { params: { ...query, subject_did: dids[0] } })
+    const pe = await $axios.$get("/api/wallet/siopv2/credentialIssuance", { params: { ...query, subject_did: dids[0] } })
     return { pe }
   },
   methods:{
@@ -78,8 +117,8 @@ export default {
           }
     },
     async peSubmit () {
-      const peResp = await this.$axios.$post("/api/wallet/siopv2/presentationExchange", this.pe)
-      console.log("PE Response:", peResp)
+      const peResp = await this.$axios.$post("/api/wallet/siopv2/credentialIssuance", this.pe)
+      this.receivedCredentials = peResp
     }
   }
 };
