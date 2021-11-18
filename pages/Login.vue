ef130b2f9fc10afc53260ce4bdcb563eaeacb178
Othmane N <otmannj56@gmail.com>
2021-11-18 02:03:29 +0100

    password && email validation

--- pages/Login.vue
+++ pages/Login.vue
@@ -4,12 +4,18 @@
       <div>
         <h2>Login</h2>
         <p class="mt-3">Access to your wallet.</p>
+        <div>
+          
+        </div>
         <form action="" id="login-form" class="my-4" @submit.prevent="login">
+          <div :class="this.error ? 'alert alert-danger mx-5 _bounce' : 'invisible'">
+            Please verify your login credentials!
+          </div>
           <div class="my-2 _animation-fade">
             <input type="email" placeholder="E-mail" name="email" id="login-form-email" class="border rounded px-3" autocomplete="off" v-model="email">
           </div>
           <div class="my-2 _animation-fade">
-            <input type="password" placeholder="Password" name="password" id="login-form-password" class="border rounded px-3" autocomplete="off" v-model="password">
+            <input type="password" placeholder="Password" name="password" id="login-form-password" @input="pwValidation()" v-bind:class="this.validPassword === true ? 'border rounded px-3' : 'border rounded px-3 border-danger'" autocomplete="off" v-model="password" >
           </div>
           <div class="my-2">
             <button type="submit" name="submit" class="text-white border-0 rounded _animation-fade">Login</button>
@@ -32,25 +38,39 @@ export default {
   data () {
     return {
       email: "",
+      validEmail: true,
       password: "",
-      error: null
+      validPassword: true,
+      error: false
     }
   },
   methods: {
+    emailValidation (email){
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(email).toLowerCase());
+    },
+    pwValidation (){
+      this.password.length > 0 ? this.validPassword = true : this.validPassword = false
+    },
     async login () {
-      try {
-        const loginResponse = await this.$auth.loginWith("local", {
-          data: {
+      if(this.emailValidation(this.email) && this.password.length > 0){
+        try {
+          const loginResponse = await this.$auth.loginWith("local", {
+            data: {
             email: this.email,
             password: this.password
-          }
-        })
-        this.$auth.setUser(loginResponse.data)
-        this.$router.push("/dashboard")
-      } catch (e) {
-        this.error = e.response.data.message
+           }
+          })
+          this.$auth.setUser(loginResponse.data)
+          this.$router.push("/dashboard")
+        } catch (e) {
+          this.error = e.response.data.message
+        }
       }
-    }
+      else{
+        this.error = true
+      }
+    },
   }
 }
 </script>
