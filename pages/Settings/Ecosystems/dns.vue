e20515c59fb38adaa761769fa4ba8b467f474b58
Othmane N <otmannj56@gmail.com>
2022-02-21 17:59:55 +0100

    fix ebsi UI, Making did:web UI, integrate the API

--- pages/Settings/Ecosystems/dns.vue
+++ pages/Settings/Ecosystems/dns.vue
@@ -10,29 +10,28 @@
             </div>
             <div class="_content justify-content-center d-flex align-items-center ">
                 <div id="content" class="_setting">
-                    <h2>EBSI/ESSIF</h2>
+                    <h2>DNS (did:web)</h2>
                     <!--<p class="mt-3">You have no Ecosystems.</p>-->
                     <div class="d-flex align-items-center justify-content-center _wizard mt-3">
                         <div :class="this.wizardIndex === 0 ? '': 'animate__fadeOutRight hide'">
                             <div class="_item">
                               <h4>Step 1</h4>
-                              <p>Copy a security token from the EBSI website.</p>
+                              <p>Create & register your did:web.</p>
                             </div>
                             <div class="mt-3 d-flex _button-view justify-content-center">
                               <a class="_bounce btn" @click="wizardNext">Get started</a>
                             </div>
-                            <p :class="this.wizardIndex === 2 ? 'hide': '_help mt-3'">Get token for the EBSI<br>Pre Production environment <span><a href="https://app.preprod.ebsi.eu/users-onboarding" target="_blank">here</a></span></p>
                         </div>
                         <div :class="this.wizardIndex === 1 ? '_left-fade': 'hide'">
                             <div class="_item">
                               <h4>Step 2</h4>
-                              <p>Insert the token below</p>
+                              <p>Define key and domain. On default a new key will be generated and the did:web will be hosted at walt.id.</p>
                             </div>
                             <div class="mt-3 d-flex _button-view justify-content-center">
                                 <div>
-                                    <form action="" id="token-submit" @submit.prevent="tokenSubmit">
-                                        <input placeholder="Insert your token" id="inserted-token" name="insertedToken" :class="this.tokenWrong===true ? 'form-control my-2 border-danger':'form-control my-2'" :data="this.token" v-model="token"/>
-                                        <button type="submit" name="submit" class="_bounce btn text-white">Submit</button>
+                                    <form action="" id="token-submit" @submit.prevent="DIDgenerate">
+                                        <input placeholder="Insert the domain …" id="inserted-doamin" name="insertedDomain" :class="this.tokenWrong===true ? 'form-control my-2 border-danger':'form-control my-2'" :data="this.domain" v-model="domain"/>
+                                        <button type="submit" name="submit" class="_bounce btn text-white">Generate DID</button>
                                     </form>
                                 </div>
                             </div>
@@ -42,7 +41,11 @@
                               <div class="success-animation">
                                   <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none" /><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" /></svg>
                               </div>
-                              <p>You just successfully onboarded on EBSI</p>
+                              <p><strong>Successfully created</strong></p>
+                              <p>Host the did:web at:</p>
+                              <a href="#">{{this.didHost}}</a>
+                              <a type="submit" name="submit" class="_bounce btn _btn-blue text-white mt-2">Download DID</a>
+                              <a type="submit" name="submit" class="_bounce btn _btn-blue text-white mt-2">Done</a>
                             </div>
                         </div>
                     </div>
@@ -83,7 +86,9 @@ export default {
       wizardIndex: 0,
       token: '',
       tokenSubmitted: false,
-      tokenWrong: false
+      tokenWrong: false,
+      domain: '',
+      didHost: ''
     }
   },
   methods:{
@@ -101,14 +106,17 @@ export default {
     wizardNext: function(){
         this.wizardIndex = this.wizardIndex+1
     },
-    async tokenSubmit (){
+    async DIDgenerate (){
         try{
-            const data = await this.$axios.$post('/api/wallet/did/create?method=ebsi', {
-                "bearerToken": this.token
+            const data = await this.$axios.$post('/api/wallet/did/create', {
+                "method": 'web',
+                "didWebDomain": this.domain
             })
-            console.log(data.data)
+            console.log(data)
+            this.didHost = data
             this.tokenSubmitted=true
             this.wizardIndex = this.wizardIndex+1
+            
         }catch(e){
             console.warn(e)
             this.tokenWrong=true
