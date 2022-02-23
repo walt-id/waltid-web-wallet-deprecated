40eaa2b5c6519c381420b4288035d001d69cda26
Othmane N <otmannj56@gmail.com>
2022-02-23 18:47:19 +0100

    Edite text for notice

--- components/Notice.vue
+++ components/Notice.vue
@@ -4,7 +4,7 @@
           <div class="d-flex col-12">
             <div class="col"></div>
             <div class="col-10 text-center text-white d-flex align-items-center justify-content-center">
-                <p><img src="https://i.ibb.co/Vm3CN36/favicon-white.png" /><strong>Notice:</strong> this walt.id version is just demo</p>
+                <p><img src="https://i.ibb.co/Vm3CN36/favicon-white.png" /><strong>Notice:</strong> Notice: This implementation is intended for demo purposes only. It should not be used in production settings.</p>
             </div>
             <div class="col text-white d-flex justify-content-end align-items-center">
                 <a @click="hideNotice">
