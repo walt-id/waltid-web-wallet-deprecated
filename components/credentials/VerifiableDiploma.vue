ee014ac5f6016853ff65a249fb92e86fc2488c5c
Philipp Potisk <phil@walt.id>
2022-03-12 19:52:02 +0100

    Formatted VerifiableDiploma.vue

--- components/credentials/VerifiableDiploma.vue
+++ components/credentials/VerifiableDiploma.vue
@@ -1,58 +1,59 @@
 <template>
   <div class="p-4">
     <div class="col-12 py-3">
-                            <span>
-                                <h5>{{$t('CREDENTIAL.TITLE')}}</h5>
-                                <p>{{$t('CREDENTIAL.TYPE.' + credential.type[credential.type.length-1])}}</p>
-                            </span>
+      <span>
+          <h5>{{$t('CREDENTIAL.TITLE')}}</h5>
+          <p>{{$t('CREDENTIAL.TYPE.' + credential.type[credential.type.length-1])}}</p>
+      </span>
     </div>
     <div class="d-flex py-3">
-                            <span class="col-4">
-                                <h5>{{$t('CREDENTIAL.ISSUER')}}</h5>
-                                <p class="text-truncate" style="max-width: 12em">{{credential.issuerName ? credential.issuerName : credential.issuer}}</p>
-                            </span>
       <span class="col-4">
-                            </span>
+          <h5>{{$t('CREDENTIAL.ISSUER')}}</h5>
+          <p class="text-truncate" style="max-width: 12em">{{credential.issuerName ? credential.issuerName : credential.issuer}}</p>
+      </span>
+      <span class="col-4"></span>
       <span class="col-4">
-                                <h5>{{$t('CREDENTIAL.STATUS')}}</h5>
-                                <p>Valid</p>
-                            </span>
+          <h5>{{$t('CREDENTIAL.STATUS')}}</h5>
+          <p>Valid</p>
+      </span>
     </div>
     <div class="d-flex py-3 border-bottom mb-3 border-secondary">
-                            <span class="col-4">
-                                <h5>{{$t('CREDENTIAL.ISSUED_ON')}}</h5>
-                                <p>{{moment(credential.issuanceDate).format("MMM Do YYYY")}}</p>
-                            </span>
       <span class="col-4">
-                                <h5>{{$t('CREDENTIAL.EXPIRE')}}</h5>
-                                <p>{{credential.expirationDate ? moment(credential.expirationDate).format("MMM Do YYYY") : "Never"}}</p>
-                            </span>
+          <h5>{{$t('CREDENTIAL.ISSUED_ON')}}</h5>
+          <p>{{moment(credential.issuanceDate).format("MMM Do YYYY")}}</p>
+      </span>
       <span class="col-4">
-                                <h5>{{$t('CREDENTIAL.VALID_FROM')}}</h5>
-                                <p>{{moment(credential.validFrom).format("MMM Do YYYY")}}</p>
-                            </span>
+          <h5>{{$t('CREDENTIAL.EXPIRE')}}</h5>
+          <p>{{credential.expirationDate ? moment(credential.expirationDate).format("MMM Do YYYY") : "Never"}}</p>
+      </span>
+      <span class="col-4">
+          <h5>{{$t('CREDENTIAL.VALID_FROM')}}</h5>
+          <p>{{moment(credential.validFrom).format("MMM Do YYYY")}}</p>
+      </span>
     </div>
     <div class="col-12 py-3">
       <div class="_image" style="background-image: url('https://www.urbanpedaltours.com/wp-content/uploads/2019/04/team.png');"></div>
     </div>
     <div class="text-left">
-                            <span class="col-12 pb-3">
-                                <h5>{{$t('CREDENTIAL.FIRST_NAME')}}</h5>
-                                <p>{{credential.credentialSubject.givenNames}}</p>
-                            </span>
+      <span class="col-12 pb-3">
+          <h5>{{$t('CREDENTIAL.FIRST_NAME')}}</h5>
+          <p>{{credential.credentialSubject.givenNames}}</p>
+      </span>
       <span class="col-12 px-3">
-                                <h5>{{$t('CREDENTIAL.LAST_NAME')}}</h5>
-                                <p>{{credential.credentialSubject.familyName}}</p>
-                            </span>
+          <h5>{{$t('CREDENTIAL.LAST_NAME')}}</h5>
+          <p>{{credential.credentialSubject.familyName}}</p>
+      </span>
       <span class="col-12 px-3">
-                                <h5>{{$t('CREDENTIAL.DATE_OF_BIRTH')}}</h5>
-                                <p>{{credential.credentialSubject.dateOfBirth}}</p>
-                            </span>
+          <h5>{{$t('CREDENTIAL.DATE_OF_BIRTH')}}</h5>
+          <p>{{credential.credentialSubject.dateOfBirth}}</p>
+      </span>
       <!-- TODO: add more properties for VerifiableDiploma -->
       <span class="col-12 px-3">
-                                <h5>{{$t('CREDENTIAL.DID')}}</h5>
-                                <p>{{credential.credentialSubject.id}}</p>
-                            </span>
+          <h5>{{$t('CREDENTIAL.DID')}}</h5>
+          <p>{{credential.credentialSubject.id}}</p>
+      </span>
+
+
     </div>
   </div>
 </template>
