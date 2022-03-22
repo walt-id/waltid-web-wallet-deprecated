b5312ad7495b0d8bca737e348d44ee5c17ebf749
mikeplotean <mike@walt.id>
2022-03-22 21:21:53 +0200

    added credential header

---
+++ components/credentials/CredentialHeader.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="p-4">
+    <div class="col-12 py-3">
+      <span>
+          <h5>{{$t('CREDENTIAL.TITLE')}}</h5>
+          <p>{{$t('CREDENTIAL.TYPE.' + credential.type[credential.type.length-1])}}</p>
+      </span>
+    </div>
+    <div class="d-flex py-3">
+      <span class="col-4">
+          <h5>{{$t('CREDENTIAL.ISSUER')}}</h5>
+          <p class="text-truncate" style="max-width: 12em">{{credential.issuerName ? credential.issuerName : credential.issuer}}</p>
+      </span>
+      <span class="col-4"></span>
+      <span class="col-4">
+          <h5>{{$t('CREDENTIAL.STATUS')}}</h5>
+          <p>Valid</p>
+      </span>
+    </div>
+    <div class="d-flex py-3 border-bottom mb-3 border-secondary">
+      <span class="col-4">
+          <h5>{{$t('CREDENTIAL.ISSUED_ON')}}</h5>
+          <p>{{moment(credential.issuanceDate).format("MMM Do YYYY")}}</p>
+      </span>
+      <span class="col-4">
+          <h5>{{$t('CREDENTIAL.EXPIRE')}}</h5>
+          <p>{{credential.expirationDate ? moment(credential.expirationDate).format("MMM Do YYYY") : "Never"}}</p>
+      </span>
+      <span class="col-4">
+          <h5>{{$t('CREDENTIAL.VALID_FROM')}}</h5>
+          <p>{{moment(credential.validFrom).format("MMM Do YYYY")}}</p>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  name: "CredentialHeader",
+  props: {
+    credential: {}
+  },
+  data() {
+    return {
+      moment: moment
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- components/credentials/VerifiableDiploma.vue
+++ components/credentials/VerifiableDiploma.vue
@@ -1,36 +1,6 @@
 <template>
   <div class="p-4">
-    <div class="col-12 py-3">
-      <span>
-          <h5>{{$t('CREDENTIAL.TITLE')}}</h5>
-          <p>{{$t('CREDENTIAL.TYPE.' + credential.type[credential.type.length-1])}}</p>
-      </span>
-    </div>
-    <div class="d-flex py-3">
-      <span class="col-4">
-          <h5>{{$t('CREDENTIAL.ISSUER')}}</h5>
-          <p class="text-truncate" style="max-width: 12em">{{credential.issuerName ? credential.issuerName : credential.issuer}}</p>
-      </span>
-      <span class="col-4"></span>
-      <span class="col-4">
-          <h5>{{$t('CREDENTIAL.STATUS')}}</h5>
-          <p>Valid</p>
-      </span>
-    </div>
-    <div class="d-flex py-3 border-bottom mb-3 border-secondary">
-      <span class="col-4">
-          <h5>{{$t('CREDENTIAL.ISSUED_ON')}}</h5>
-          <p>{{moment(credential.issuanceDate).format("MMM Do YYYY")}}</p>
-      </span>
-      <span class="col-4">
-          <h5>{{$t('CREDENTIAL.EXPIRE')}}</h5>
-          <p>{{credential.expirationDate ? moment(credential.expirationDate).format("MMM Do YYYY") : "Never"}}</p>
-      </span>
-      <span class="col-4">
-          <h5>{{$t('CREDENTIAL.VALID_FROM')}}</h5>
-          <p>{{moment(credential.validFrom).format("MMM Do YYYY")}}</p>
-      </span>
-    </div>
+    <CredentialHeader :credential="credential"></CredentialHeader>
     <div class="col-12 py-3">
       <div class="_image" style="background-image: url('https://www.urbanpedaltours.com/wp-content/uploads/2019/04/team.png');"></div>
     </div>
@@ -74,6 +44,7 @@
 
 <script>
 import moment from "moment";
+import CredentialHeader from './CredentialHeader.vue';
 
 export default {
   name: "VerifiableDiploma",
@@ -84,6 +55,9 @@ export default {
     return {
       moment: moment
     }
+  },
+  components: {
+    CredentialHeader
   }
 }
 </script>
