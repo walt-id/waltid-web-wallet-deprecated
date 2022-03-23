3411949358ab703e703576c51598731686243ffd
Philipp Potisk <phil@walt.id>
2022-03-23 09:01:08 +0100

    Corrected proxy setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,8 +60,8 @@ export default {
   },
 
   proxy: {
-   //"/api/": "https://wallet.waltid.org"
-   "/api/": "http://localhost:8080"
+   "/api/": "https://wallet.waltid.org"
+   //"/api/": "http://localhost:8080"
   },
   auth: {
     strategies: {
