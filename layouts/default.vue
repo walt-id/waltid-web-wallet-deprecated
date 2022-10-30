f8b851578f2b8e6fcb69f20eb321ace3c1d4dd8a
Taha <farah.aidid@outlook.com>
2022-10-30 15:18:45 +0600

    issue #87 icon added, and primary setup of modal

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,14 +3,22 @@
     <Notice></Notice>
     <section class="_main bg-light row align-items-center justify-content-center justify-content-lg-start justify-content-md-center justify-content-sm-center">
     <div id="widget" class="_form d-grid align-items-center bg-w shadow-lg text-center">
-      <div class="_toggle-menu position-sticky d-flex justify-content-end col-12 align-items-center px-3">
+      <div class="full-page-modal" v-if="showFullPageModal">
+        <span class="close-icon" @click="showFullPageModal = false">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M6 18L18 6M6 6L18 18" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+        </span>
+        <Nuxt />
+      </div>
+      <div v-if="!showFullPageModal" class="_toggle-menu position-sticky d-flex justify-content-end col-12 align-items-center px-3">
           <a id="toggle-menu" @click="menuTrigger" v-if="!hideMenu">
               <div id="dash-1" class="_dash my-2"></div>
               <div id="dash-2" class="_dash my-2"></div>
               <div id="dash-3" class="_dash my-2"></div>
           </a>
       </div>
-      <div class="_content justify-content-center d-flex align-items-center" style="margin-top: -2em">
+      <div v-if="!showFullPageModal" class="_content justify-content-center d-flex align-items-center" style="margin-top: -2em">
         <div id="content">
           <Nuxt />
         </div>
@@ -34,7 +42,7 @@
           </ul>
         </div>
       </div>
-      <div class="_copyright _blue-color d-flex align-items-center justify-content-center">
+      <div v-if="!showFullPageModal" class="_copyright _blue-color d-flex align-items-center justify-content-center">
         <a id="copyright" href="https://walt.id/" target="_blank">{{copyright}}</a>
       </div>
     </div>
@@ -53,7 +61,8 @@ export default {
     return {
       copyright: config.copyright,
       trigger: true,
-      config: config
+      config: config,
+      showFullPageModal: true,
     }
   },
   computed: {
@@ -75,6 +84,14 @@ export default {
         this.menuTrigger()
     }
   },
+  mounted(){
+    this.$nuxt.$on('toggle-full-page-modal', () => {
+      this.showFullPageModal = true
+    })
+  },
+  beforeUnmount(){
+    this.$nuxt.$off('toggle-full-page-modal')
+  },
   methods:{
     menuTrigger: function(){
       console.log(this.$route)
@@ -94,3 +111,13 @@ export default {
   }
 };
 </script>
+
+<style lang="sass">
+.full-page-modal
+  height: 100%
+  position: relative
+  .close-icon
+    position: absolute
+    right: 1rem
+    top: 0.5rem
+</style>
--- pages/nfts/_chain/index.vue
+++ pages/nfts/_chain/index.vue
@@ -3,7 +3,14 @@
         <h2 class="_animation-fade">{{$t('NFTS.TITLE')}}</h2>
         <div class="_search">
             <form action="" id="search-form">
-                <input name="search-input" type="text" v-model="search" :placeholder="$t('NFTS.SEARCH')">
+                <div class="d-flex align-items-center justify-content-center" style="gap: 0.3rem">
+                  <input name="search-input" type="text" v-model="search" :placeholder="$t('NFTS.SEARCH')">
+                  <span @click="$nuxt.$emit('toggle-full-page-modal')">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" style="width: 42px; height: 42px;">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
+                    </svg>
+                  </span>
+                </div>
             </form>
             <b-form-select v-model="chain" :options="chainOptions"/>
         </div>
