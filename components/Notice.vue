458e8a60e81859d3ae27ef2e7c9184fd0edc8087
KBurgmann <kb@gatgetron.net>
2023-03-27 00:50:05 +0200

    Updated background image, removed nagbar

--- components/Notice.vue
+++ components/Notice.vue
@@ -3,9 +3,6 @@
         <div class="container-fluid">
           <div class="d-flex col-12">
             <div class="col"></div>
-            <div class="col-10 text-center text-white d-flex align-items-center justify-content-center">
-                <p><img :src="this.logo.path" :alt="this.logo.alt" /><strong>Notice:</strong> This implementation is intended for demo purposes only. It should not be used in production settings.</p>
-            </div>
             <div class="col text-white d-flex justify-content-end align-items-center">
                 <a @click="hideNotice">
                     <i class="bi bi-x-lg"></i>
