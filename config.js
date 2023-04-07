ec9e86df228035357172606985218f285d4d0b47
aminbenmansour <amin.benmansour.10@gmail.com>
2023-04-07 05:30:05 +0100

    add nft marketplace for unique network

--- config.js
+++ config.js
@@ -27,6 +27,7 @@ const config = {
         "POLYGON": "https://opensea.io/assets/matic/",
         "ETHEREUM": "https://opensea.io/assets/ethereum/",
         "TEZOS": "https://rarible.com/token/tezos/",
+        "Unique": "https://unqnft.io/market",
         "mainnet": ""
     }
 }
